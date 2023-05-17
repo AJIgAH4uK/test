ae03f195ad473c3512859741863f4272bcfdab2e
AJIgAH4uK <AJIgAH4uK@gmail.com>
2023-05-17 09:00:12 +0300

    отказался от Extension

--- local/js/custom/swiper/config.php
+++
@@ -1,8 +0,0 @@
-<?if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED !== true) die();
-
-return [
-	"css" => "/local/js/custom/swiper/swiper-bundle.css",
-	"js" => [
-		"/local/js/custom/swiper/swiper-bundle.js",
-	],
-];
