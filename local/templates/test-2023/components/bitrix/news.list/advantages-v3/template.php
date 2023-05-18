c5c1ab3d3f11cc0c25f19fa29e44702e67bf297e
AJIgAH4uK <AJIgAH4uK@gmail.com>
2023-05-19 01:07:37 +0300

    закончил шаблоны компонентов

--- local/templates/test-2023/components/bitrix/news.list/advantages-v3/template.php
+++ local/templates/test-2023/components/bitrix/news.list/advantages-v3/template.php
@@ -14,54 +14,43 @@ $this->setFrameMode(true);
 ?>
 <section class="advantages-v3">
     <div class="content-center">
-        <div class="title">Когда вы оформите заявку:</div>
+        <div class="title"><?=$arResult['NAME']?></div>
         <div class="columns">
-            <div class="column">
-                <div class="num-1" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
-                    <div class="sub-title">Личный менеджер</div>
-                    <div class="description">Вам назначат личного менеджера. Он&nbsp;запросит нужные документы и&nbsp;составит
-                        договор
-                        на&nbsp;брокерское обслуживание.
-                    </div>
+                <div class="column">
+                    <? foreach ($arResult['ITEMS'] as $key => $arItem):?>
+                    <?
+                    $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+                    $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+                    ?>
+                        <? if($key < 3):?>
+                        <div class="num-<?=$key + 1?>" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
+                            <div class="sub-title"><?=$arItem['NAME']?></div>
+                            <div class="description"><?=$arItem['PREVIEW_TEXT']?></div>
+                            <? if(!empty($arItem['DETAIL_TEXT'])):?>
+                            <div class="info">
+                                <?=$arItem['DETAIL_TEXT']?>
+                            </div>
+                            <? endif?>
+                        </div>
+                        <? endif?>
+                    <?endforeach?>
                 </div>
-                <div class="num-2">
-                    <div class="sub-title">Документы</div>
-                    <div class="description">Юридический отдел поможет вам собрать документы для таможни и&nbsp;рассчитает
-                        таможенный сбор.
-                    </div>
-                </div>
-                <div class="num-3">
-                    <div class="sub-title">Транспортировка</div>
-                    <div class="description">Отдел логистики доставит товар с&nbsp;точки отгрузки на&nbsp;таможенный
-                        склад и&nbsp;организует досмотр.
-                    </div>
-                    <div class="info">
-                        У&nbsp;вашего личного менеджера всегда есть информация
-                        о&nbsp;местонахождении груза и&nbsp;ожидаемом сроке прибытия.
-                    </div>
-                </div>
-            </div>
-            <div class="column">
-                <div class="num-4">
-                    <div class="sub-title">Таможня</div>
-                    <div class="description">Юридический отдел подаст документы
-                        в&nbsp;таможню, решит возникшие вопросы и&nbsp;уплатит таможенные сборы.
-                    </div>
-                </div>
-                <div class="num-5">
-                    <div class="sub-title">Оплата</div>
-                    <div class="description">Когда груз пройдет таможенную очистку и&nbsp;будет выпущен с&nbsp;таможенного
-                        склада, личный менеджер свяжется с&nbsp;вами и&nbsp;выставит счет.
-                    </div>
-                </div>
-                <div class="num-6">
-                    <div class="sub-title">Доставка</div>
-                    <div class="description">После оплаты счета груз будет доставлен
-                        по&nbsp;указанному адресу, а&nbsp;вы&nbsp;получите полный набор документов, легализующих
-                        пребывание груза в&nbsp;России, включая лицензии
-                        и&nbsp;сертификаты.
-                    </div>
-                </div>
+                <?if(count ($arResult['ITEMS']) > 3):?>
+                        <div class="column">
+                            <? foreach ($arResult['ITEMS'] as $key => $arItem):?>
+                        <?
+                        $this->AddEditAction($arItem['ID'], $arItem['EDIT_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_EDIT"));
+                        $this->AddDeleteAction($arItem['ID'], $arItem['DELETE_LINK'], CIBlock::GetArrayByID($arItem["IBLOCK_ID"], "ELEMENT_DELETE"), array("CONFIRM" => GetMessage('CT_BNL_ELEMENT_DELETE_CONFIRM')));
+                        ?>
+                            <? if($key > 2):?>
+                                <div class="num-<?=$key + 1?>" id="<?= $this->GetEditAreaId($arItem['ID']); ?>">
+                                    <div class="sub-title"><?=$arItem['NAME']?></div>
+                                    <div class="description"><?=$arItem['PREVIEW_TEXT']?></div>
+                                </div>
+                            <?endif?>
+                        <? endforeach;?>
+                        </div>
+                <? endif?>
             </div>
         </div>
     </div>
